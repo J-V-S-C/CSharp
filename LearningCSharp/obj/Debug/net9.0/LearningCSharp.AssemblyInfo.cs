1128bf21c6dbaf24c41d0052fa355445a39cc1aa
João Victor Sant'Ana Cortabitart <joaovictorcortabitart@gmail.com>
2025-02-12 19:12:21 -0300

    ooooo

--- LearningCSharp/obj/Debug/net9.0/LearningCSharp.AssemblyInfo.cs
+++ LearningCSharp/obj/Debug/net9.0/LearningCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32439f1a0d71a5df977dc2f1faea9a3983ff045a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccbdc1378a999900877dedc0596fa450adc90a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
